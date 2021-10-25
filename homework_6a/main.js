4ff32bfb107c09261e1581d33a80b10f35abba87
Roshan Selladurai <rselladu@cs.cmu.edu>
2021-10-24 20:35:34 -0400

    Validators
    Ran the files through validator and fixed minor issues

--- homework_6a/main.js
+++ homework_6a/main.js
@@ -1,3 +1,4 @@
+//Adds the number of items to the cart icon//
 function incrementCartValue() {
   var quantity = parseInt(document.getElementById('quant').value);
   var value = parseInt(document.getElementById('number').value, 10);
@@ -18,6 +19,7 @@ function incrementCartValue() {
     
 }
 
+//Adds the quantity box on the product details page//
 function incrementQuantValue() {
   var value = parseInt(document.getElementById('quant').value, 10);
   value = isNaN(value) ? 0 : value;
@@ -26,6 +28,7 @@ function incrementQuantValue() {
 
 }
 
+//Decreases the quantity box on the product details page//
 function decreaseValue() {
   var value = parseInt(document.getElementById('quant').value, 10);
   value = value === 0 ? 0 : value;
@@ -39,114 +42,127 @@ function decreaseValue() {
     
 }
 
+//Displayes Color Selected: Strawberry when clicked//
 function displayStrawberry() {
   var text = document.getElementById('color');
   
-  if (text.innerHTML === "Strawberry") {
-    document.getElementById('color').innerHTML = "";
+  if (text.innerHTML === "Color Selected: Strawberry") {
+    document.getElementById('color').innerHTML = "Select Color:";
   } else {
-    document.getElementById('color').innerHTML = "Strawberry";
+    document.getElementById('color').innerHTML = "Color Selected: Strawberry";
   }
     
 }
 
+//Displayes Color Selected: Blackberry when clicked//
 function displayBlackberry() {
   var text = document.getElementById('color');
   
-  if (text.innerHTML === "Blackberry") {
-    document.getElementById('color').innerHTML = "";
+  if (text.innerHTML === "Color Selected: Blackberry") {
+    document.getElementById('color').innerHTML = "Select Color:";
   } else {
-    document.getElementById('color').innerHTML = "Blackberry";
+    document.getElementById('color').innerHTML = "Color Selected: Blackberry";
   }
     
 }
 
+//Displayes Color Selected: Cranberry when clicked//
 function displayCranberry() {
   var text = document.getElementById('color');
   
-  if (text.innerHTML === "Cranberry") {
-    document.getElementById('color').innerHTML = "";
+  if (text.innerHTML === "Color Selected: Cranberry") {
+    document.getElementById('color').innerHTML = "Select Color:";
   } else {
-    document.getElementById('color').innerHTML = "Cranberry";
+    document.getElementById('color').innerHTML = "Color Selected: Cranberry";
   }
     
 }
 
+//Displayes Color Selected: Fire Orange when clicked//
 function displayFireOrange() {
   var text = document.getElementById('color');
   
-  if (text.innerHTML === "Fire Orange") {
-    document.getElementById('color').innerHTML = "";
+  if (text.innerHTML === "Color Selected: Fire Orange") {
+    document.getElementById('color').innerHTML = "Select Color:";
   } else {
-    document.getElementById('color').innerHTML = "Fire Orange";
+    document.getElementById('color').innerHTML = "Color Selected: Fire Orange";
   }
     
 }
 
+//Displayes Size Selected: Tiny when clicked//
 function displayTiny() {
   var text = document.getElementById('size');
   
-  if (text.innerHTML === "Tiny") {
-    document.getElementById('size').innerHTML = "";
+  if (text.innerHTML === "Size Selected: Tiny") {
+    document.getElementById('size').innerHTML = "Select Size:";
   } else {
-    document.getElementById('size').innerHTML = "Tiny";
+    document.getElementById('size').innerHTML = "Size Selected: Tiny";
   }
     
 }
 
+//Displayes Size Selected: Small when clicked//
 function displaySmall() {
   var text = document.getElementById('size');
   
-  if (text.innerHTML === "Small") {
-    document.getElementById('size').innerHTML = "";
+  if (text.innerHTML === "Size Selected: Small") {
+    document.getElementById('size').innerHTML = "Select Size:";
   } else {
-    document.getElementById('size').innerHTML = "Small";
+    document.getElementById('size').innerHTML = "Size Selected: Small";
   }
     
 }
 
+//Displayes Size Selected: Medium when clicked//
 function displayMedium() {
   var text = document.getElementById('size');
   
-  if (text.innerHTML === "Medium") {
-    document.getElementById('size').innerHTML = "";
+  if (text.innerHTML === "Size Selected: Medium") {
+    document.getElementById('size').innerHTML = "Select Size:";
   } else {
-    document.getElementById('size').innerHTML = "Medium";
+    document.getElementById('size').innerHTML = "Size Selected: Medium";
   }
     
 }
 
+//Displayes Size Selected: Large when clicked//
 function displayLarge() {
   var text = document.getElementById('size');
   
-  if (text.innerHTML === "Large") {
-    document.getElementById('size').innerHTML = "";
+  if (text.innerHTML === "Size Selected: Large") {
+    document.getElementById('size').innerHTML = "Select Size:";
   } else {
-    document.getElementById('size').innerHTML = "Large";
+    document.getElementById('size').innerHTML = "Size Selected: Large";
   }
     
 }
 
+//Changes main image to be Strawberry harness when selected//
 function changeImageStraw() {
   document.getElementById("productphoto").src = "images/strawberry.png";  
 
 }
 
+//Changes main image to be Blackberry harness when selected//
 function changeImageBlack() {
   document.getElementById("productphoto").src = "images/blackberry.png";  
 
 }
 
+//Changes main image to be Cranberry harness when selected//
 function changeImageCran() {
   document.getElementById("productphoto").src = "images/cranberry.png";  
 
 }
 
+//Changes main image to be Fire Orange harness when selected//
 function changeImageFire() {
   document.getElementById("productphoto").src = "images/fireorange.png";  
 
 }
 
+//Changed opacity on quant/add to cart buttons once color and size have been selected//
 function activateButtons() {
   var colorpick = document.getElementById('color').innerHTML;
   
