a5afc382860f05e1822c705515899d37c330b493
Roshan Selladurai <rselladu@cs.cmu.edu>
2021-10-24 22:23:32 -0400

    Added Files and Assets Folder
    Added the low-fi and high-fi writeups. Also added Assets folder

--- homework_6a/main.js
+++ homework_6a/main.js
@@ -140,25 +140,25 @@ function displayLarge() {
 
 //Changes main image to be Strawberry harness when selected//
 function changeImageStraw() {
-  document.getElementById("productphoto").src = "images/strawberry.png";  
+  document.getElementById("productphoto").src = "Assets/images/strawberry.png";  
 
 }
 
 //Changes main image to be Blackberry harness when selected//
 function changeImageBlack() {
-  document.getElementById("productphoto").src = "images/blackberry.png";  
+  document.getElementById("productphoto").src = "Assets/images/blackberry.png";  
 
 }
 
 //Changes main image to be Cranberry harness when selected//
 function changeImageCran() {
-  document.getElementById("productphoto").src = "images/cranberry.png";  
+  document.getElementById("productphoto").src = "Assets/images/cranberry.png";  
 
 }
 
 //Changes main image to be Fire Orange harness when selected//
 function changeImageFire() {
-  document.getElementById("productphoto").src = "images/fireorange.png";  
+  document.getElementById("productphoto").src = "Assets/images/fireorange.png";  
 
 }
 
