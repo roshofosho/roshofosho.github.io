0e79dc262abd900afb0a0a6dab66e5eebe1b08bc
Roshan Selladurai <rselladu@cs.cmu.edu>
2021-11-08 23:32:54 -0500

    added photochange
    added photochange.js

---
+++ homework_6b/photochange.js
@@ -0,0 +1,23 @@
+//Changes main image to be Strawberry harness when selected//
+function changeImageStraw() {
+  document.getElementById("productphoto").src = "Assets/images/strawberry.png";  
+
+}
+
+//Changes main image to be Blackberry harness when selected//
+function changeImageBlack() {
+  document.getElementById("productphoto").src = "Assets/images/blackberry.png";  
+
+}
+
+//Changes main image to be Cranberry harness when selected//
+function changeImageCran() {
+  document.getElementById("productphoto").src = "Assets/images/cranberry.png";  
+
+}
+
+//Changes main image to be Fire Orange harness when selected//
+function changeImageFire() {
+  document.getElementById("productphoto").src = "Assets/images/fireorange.png";  
+
+}
